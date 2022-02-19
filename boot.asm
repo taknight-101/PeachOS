71ffcf05c05d9e641023d1e8db4e9958e78d294b
Ahmed Ibrahim <taknight101.work@protonmail.com>
2022-02-19 18:32:49 +0200

    Wrote and invoked my custom interrupt that displays the letter 'V' on the screen

--- boot.asm
+++ boot.asm
@@ -1,8 +1,8 @@
-ORG 0x7c00
+ORG 0x7c00 
+; ORG 0 
 BITS 16
 
-
-
+; some bios needs this stub to pad with zeros as they overwrite the bios block param structure 
 _start:
     jmp short start
     nop
@@ -12,6 +12,15 @@ times 33 db 0
 start:
     jmp 0:step2
 
+; 
+
+handle_zero:
+    mov ah , 0eh
+    mov al , 'V' 
+    mov bx , 0x00
+    int 0x10
+    iret
+
 step2:
     cli ; Clear Interrupts
     mov ax, 0x00
@@ -20,7 +29,15 @@ step2:
     mov ss, ax
     mov sp, 0x7c00
     sti ; Enables Interrupts
-    
+   
+    mov word[ss:0x00] , handle_zero - 0x7c00 ; this took some time from me to get on my own :)
+    mov word[ss:0x02] , 0x7c0
+
+    mov ax, 0x00
+    div ax
+    ; or int 0
+    int 0 
+
     mov si, message
     call print
     jmp $
@@ -46,5 +63,5 @@ print_char:
 
 message: db 'Hello World!', 0
 
-times 510-($ - $$) db 0
-dw 0xAA55 
+times 510-($ - $$) db 0 ; as the BIOS will look for the bootloader's code in the first sector "first 521 bytes" in every storage medium
+dw 0xAA55  ; boot signature that marks the sector as bootable <contains the bootloader code> this sector later on gets loaded at address 0x7C00 to be executed
